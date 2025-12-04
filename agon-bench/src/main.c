a6cbeef14771b3a95812823ebad1f5b21a14b1e0
Tom Morton <tomm@riseup.net>
2025-12-04 14:15:51 +0000

    Updated agon-bench (1.05) to AgonDev

--- agon-bench/src/main.c
+++ agon-bench/src/main.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <mos_api.h>
 #include <agon/vdp_vdu.h>
 
 typedef void (*vblank_handler_t)();
 
-volatile int32_t count = 0;
+/* Pointer to sysvar_time. Gets +=2 at a 60Hz freq */
+int24_t * volatile pCount;
 
-// vblank.asm
-extern void vblank_handler();
-extern void fast_vdu(uint8_t *data, int len);
 // misc.asm
 extern uint8_t getsysvar_gp(void);
 extern void delay_1sec(void);
-
-// called from vblank_handler
-void on_vblank()
-{
-	count++;
-}
 
 static void vdu_gp(uint8_t msg)
 {
@@ -43,26 +36,47 @@ static void vdu_gp(uint8_t msg)
 	        "\tpop bc\n" \
 	        "\tpop af\n"
 
+#define WAIT_VBLANK() \
+	for (setCount(-2); getCount(); ) {}
+
+inline void setCount(int24_t c) {
+	*pCount = (c<<1);
+}
+
+inline int24_t getCount(void) {
+	return (*pCount)>>1;
+}
+
 int main() {
 	// mode 0: make sure we have a 60hz mode for timing
 	putch(22); putch(0);
 
-	printf("Agon Bench 1.04\n");
+	/* Get pointer to sysvar_time */
+	asm volatile(
+	"	push af\n"
+	"	push ix\n"
+	"	ld a,8  /* mos_sysvar call */ \n"
+	"	rst.lil 8\n"
+	"	ld (_pCount),ix\n"
+	"	pop ix\n"
+	"	pop af\n"
+	);
+
+	printf("Agon Bench 1.05\n");
 	printf("===============\n\n");
-	vblank_handler_t old_handler = mos_setintvector(0x32, &vblank_handler);
 
 	{
-		for (count=-1; count; ) {} // clear counter and let current frame finish
+		WAIT_VBLANK();
 		delay_1sec(); // should really be 1 second if 18.432MHz cpu
 		delay_1sec();
-		printf("ez80 frequency:                      %.3f MHz\n", 2 * AGON_CLK * 60.0f / count);
+		printf("ez80 frequency:                      %.3f MHz\n", 2 * AGON_CLK * 60.0f / getCount());
 	}
 	printf("\n");
 
 	{
-		for (count=-1; count; ) {} // clear counter and let current frame finish
+		WAIT_VBLANK();
 		int iters = 0;
-		while (count < 60) {
+		while (getCount() < 60) {
 			asm(PUSH_ALL
 			"	ld a,0x8\n"
 			"	rst.lil 0x8\n"
@@ -75,7 +89,7 @@ int main() {
 	printf("\n");
 
 	{
-		for (count=-1; count; ) {} // clear counter and let current frame finish
+		WAIT_VBLANK();
 		for (int j=0; j<10; j++) {
 			uint8_t i=0;
 			while (i<255) {
@@ -84,48 +98,39 @@ int main() {
 				i++;
 			}
 		}
-		float elapsed = (float)count / 60.0f;
+		float elapsed = (float)getCount() / 60.0f;
 		printf("ez80 <-> VDP round-trips per second  %.0f\n", 2550.0f / elapsed);
 		printf("ez80 <-> VDP round-trip latency      %.2f ms\n", 1000.0f * elapsed / 2550.0f);
 	}
 
 
 	{
-		for (count=-1; count; ) {} // clear counter and let current frame finish
+		WAIT_VBLANK();
 		// load 64KiB of junk into the vdp
 		vdp_load_bitmap(128, 128, (uint32_t*)0x40000);
-		float elapsed = (float)count / 60.0f;
+		float elapsed = (float)getCount() / 60.0f;
 		printf("ez80 to VDP                          %.2f KiB/sec (%.2f KBit/sec)\n", 64.0f / elapsed, 8.0f*64.0f / elapsed);
 	}
 
-	{
-		for (count=-1; count; ) {} // clear counter and let current frame finish
-		// load 64KiB of junk into the vdp
-		putch(23); putch(27); putch(1);
-		putch(128); putch(0);
-		putch(128); putch(0);
-		fast_vdu((uint8_t*)0x40000, 65536);
-		float elapsed = (float)count / 60.0f;
-		printf("ez80 to VDP (fast_vdu)               %.2f KiB/sec (%.2f KBit/sec)\n", 64.0f / elapsed, 8.0f*64.0f / elapsed);
-	}
 	printf("\n");
+
+	char *buf = malloc(65536);
 	
 	mos_del("agon-bench.tmp");
 	{
-		for (count=-1; count; ) {} // clear counter and let current frame finish
-		mos_save("agon-bench.tmp", 0xa0000, 65536);
-		float elapsed = (float)count / 60.0f;
+		WAIT_VBLANK();
+		mos_save("agon-bench.tmp", buf, 65536);
+		float elapsed = (float)getCount() / 60.0f;
 		printf("SDCard write                         %.2f KiB/sec\n", 64.0f / elapsed);
 	}
 	{
-		for (count=-1; count; ) {} // clear counter and let current frame finish
-		mos_load("agon-bench.tmp", 0xa0000, 65536);
-		float elapsed = (float)count / 60.0f;
+		WAIT_VBLANK();
+		mos_load("agon-bench.tmp", buf, 65536);
+		float elapsed = (float)getCount() / 60.0f;
 		printf("SDCard read                          %.2f KiB/sec\n", 64.0f / elapsed);
 	}
+	free(buf);
 	mos_del("agon-bench.tmp");
 	
-	mos_setintvector(0x32, old_handler);
-
 	return 0;
 }
